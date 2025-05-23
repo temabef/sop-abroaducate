bfe5bc192c7f033490ba6333df73f5bf3ca3de9b
temabef <temabef@gmail.com>
2025-05-23 19:42:09 +0300

    Add PostCSS and Tailwind configuration files

---
+++ sop-frontend/tailwind.config.cjs
@@ -0,0 +1,9 @@
+// tailwind.config.cjs
+/** @type {import('tailwindcss').Config} */
+module.exports = { // SvelteKit often uses .cjs for CommonJS
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
